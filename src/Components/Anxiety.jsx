7c359c7d65d646f29481854ad1079747bb8bd348
Ankit <contactankit1234@gmail.com>
2025-03-22 19:01:12 +0530

    Add footer, navbar, and GIFs

--- src/Components/Anxiety.jsx
+++ src/Components/Anxiety.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import gif1 from '../assets/gif1.gif';
+import gif2 from '../assets/gif2.gif';
 
 const Anxiety = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center justify-center">
+      <img src={gif1} alt="Anxiety GIF 1" className="w-full mb-20 h-[80vh] object-cover" />
+      <img src={gif2} alt="Anxiety GIF 2" className="w-full mb-20 h-[20vh] object-cover" />
     </div>
   );
 }
